85f829e98c0272a4dd9f9f23113ed0c43d24c9b8
Jakwes <jakwesoneilgonzalez@gmail.com>
2024-04-06 16:26:33 +0800

    practice commit

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,23 +1,29 @@
 import { useState } from "react";
-
-function ListGroup() {
-  let items=[
-    'New York','San Francisco','Tokyo','London','Paris'];
+//{items: [], heading: string}
+interface ListGroupProps {
+  items:string[];
+  heading:string;
+  //(item:string) => void
+  onSelectItem:(item:string)=> void;
+}
+function ListGroup({items, heading, onSelectItem}:ListGroupProps) {
+  
   const [selectedIndex, setSelectedIndex] = useState(-1);
-
   //Hook - allows us to tap into built in features in react
-  
-  
+
   return (
     <>
-      <h1>List</h1>
+      <h1>{heading}</h1>
       {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map((item,index)=> 
           <li 
             className= {selectedIndex == index ? 'list-group-item active' : 'list-group-item'}
             key={item} 
-            onClick={() => {setSelectedIndex(index)}}
+            onClick={() => {
+              setSelectedIndex(index);
+              onSelectItem(item);
+            }}
           >
             {item}
           </li> //on real world add item.id for the key (first item in the line)
